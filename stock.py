df3def992013d9e667d627967c0537e51b6a3fd9
Miao Xie <miaoxie7788@gmail.com>
2020-04-26 15:34:48 +1000

    Reorganise stock.py.

--- stock.py
+++ stock.py
@@ -1,18 +1,63 @@
 """
-This script is to model the relationships among the major quantitative factors that influence share prices using
-regression. Basically, it is to predict how volume, cash rate, dividend, inflation rate, and other company-specific
-data influence the share price.
+    This script is to model the relationship between share price and other quantitative indexes (QIs) such as cpi, gdp,
+    cash rate, dividend and etc.
 
-The R&D is based on Telstra Corp. Ltd. (TLS).
-
+        - cpi, gdp, unemployment rate, current account, government finance (IMF)
+                            https://www.imf.org/external/datamapper/datasets
+        - cash rate (RBA)   https://www.rba.gov.au/statistics/cash-rate/
+        - dividend (ASX)    https://www.intelligentinvestor.com.au/
+    -----------------------------------------------------------------------------------------------
+        - share (ASX)       https://www.marketindex.com.au/
 """
 
 import pandas as pd
 from sklearn import linear_model
 
-tls_ingest_params = {
+imf_filenames = {
+    'cpi': "data/imf/imf-inflation-20200422.csv",
+    'gdp': "data/imf/imf-gdp-20200422.csv",
+    'unemployment': "data/imf/imf-people-20200422.csv",
+    'current': "data/imf/imf-current account-20200422.csv",
+    'government': "data/imf/imf-government finance-20200422.csv",
+}
+
+imf_ingest_params = {
     'header': 0,
-    'index_col': False,
+    'index_col': 0,
+    'encoding': 'ISO-8859-1',
+}
+
+cash_ingest_params = {
+    'filepath_or_buffer': "data/cash_rate.csv",
+    'header': 0,
+    'index_col': 'effective_date',
+    'dtype': {'effective_date': str,
+              'change_points': float,
+              'cash_rate_target': float,
+              'related_documents': str,
+              },
+    'parse_dates': ['effective_date'],
+}
+
+div_ingest_params = {
+    'header': 0,
+    'index_col': 'ex_dividend_date',
+    'dtype': {'sector': str,
+              'market_cap': str,
+              'distribution_type': str,
+              'dividend': float,
+              'franking': str,
+              'ex_dividend_date': str,
+              'payment_date': str,
+              'current_price': str,
+              'price_7d_avg': str,
+              'dividend_yield': str,
+              },
+    'parse_dates': ['ex_dividend_date', 'payment_date'],
+}
+
+share_ingest_params = {
+    'header': 0,
     'dtype': {'date': str,
               'open': float,
               'high': float,
@@ -20,128 +65,117 @@ tls_ingest_params = {
               'close': float,
               'volume': int,
               },
-    'engine': 'c',
     'parse_dates': ['date'],
-
 }
 
-cash_rate_ingest_params = {
-    'header': 0,
-    'index_col': False,
-    'dtype': {'effective_date': str,
-              'change_points': float,
-              'cash_rate_target': float,
-              },
-    'engine': 'c',
-    'parse_dates': ['effective_date'],
 
-}
+def imf_ingest(filenames, ingest_params):
+    imfs = dict()
+    for qi_name, filename in filenames.items():
+        df = pd.read_csv(filepath_or_buffer=filename, **ingest_params)
+        qi = df.loc["Australia"]
+        qi.index = pd.to_datetime(qi.index)
+        imfs[qi_name] = qi
 
-dividend_ingest_params = {
-    'header': 0,
-    'index_col': False,
-    'dtype': {'ex_date': str,
-              'amount': float,
-              'franking': str,
-              'type': str,
-              'payable': str,
-              },
-    'engine': 'c',
-    'parse_dates': ['ex_date', 'payable'],
-}
-
-cpi_ingest_params = {
-    'header': 0,
-    'index_col': False,
-    'dtype': {'date': str,
-              'inflation_rate': float,
-              'annual_change': float,
-              },
-    'engine': 'c',
-    'parse_dates': ['date'],
-
-}
-
-tls_df = pd.read_csv(filepath_or_buffer='data/tls.csv', **tls_ingest_params)
-cash_rate_df = pd.read_csv(filepath_or_buffer='data/cash_rate.csv', **cash_rate_ingest_params)
-dividend_df = pd.read_csv(filepath_or_buffer='data/dividend.csv', **dividend_ingest_params)
-cpi_df = pd.read_csv(filepath_or_buffer='data/inflation_rate.csv', **cpi_ingest_params)
+    return imfs
 
 
-def transform_date_rate_as_dict(df, date_column, rate_column):
+def cash_ingest(ingest_params):
+    df = pd.read_csv(**ingest_params)
+    cash = df["cash_rate_target"]
+
+    return cash
+
+
+def div_ingest(asx_code, ingest_params):
+    filename = "data/{asx_code}_dividend.csv".format(asx_code=asx_code)
+    df = pd.read_csv(filepath_or_buffer=filename, **ingest_params)
+    div = df["dividend"]
+
+    return div
+
+
+def share_ingest(asx_code, ingest_params):
+    filename = "data/{asx_code}.csv".format(asx_code=asx_code)
+    df = pd.read_csv(filepath_or_buffer=filename, **ingest_params)
+
+    return df
+
+
+def get_rate(date, qi):
     """
-        Transform date, rate as dictionary from a given df.
-
-        cash_rate: effective_date, cash_rate_target
-        dividend: ex_date, amount
-        inflation_rate: date, inflation_rate
+        Get
+        If date appears exactly in ref, it returns the rate that corresponds to the date; otherwise, it returns
+        the rate that corresponds to the closet date (later) appeared in ref.
     """
-    date_rate_dict = {row[date_column]: row[rate_column] for _, row in df.iterrows()}
-
-    return date_rate_dict
-
-
-def get_rate_by_date(date, date_rate_dict):
-    """
-        If date appears in date_rate_dict, it returns the rate that corresponds to the date; otherwise, it returns
-        the rate that corresponds to the closet date (later) appeared in date_list.
-    """
-    if date in date_rate_dict:
-        rate = date_rate_dict[date]
+    if date in qi:
+        rate = qi[date]
     else:
         closet_date = None
-        for key in date_rate_dict:
+        for idx in qi.index:
             if not closet_date:
-                if (date - key).days >= 0:
-                    closet_date = key
-            elif (date - key).days >= 0 and key > closet_date:
-                closet_date = key
-        rate = date_rate_dict[closet_date]
+                if (date - idx).days >= 0:
+                    closet_date = idx
+            elif (date - idx).days >= 0 and idx > closet_date:
+                closet_date = idx
+        rate = qi[closet_date]
+
+    try:
+        rate = float(rate)
+    except TypeError:
+        rate = 0
 
     return rate
 
 
-cash_rate_dict = transform_date_rate_as_dict(cash_rate_df, 'effective_date', 'cash_rate_target')
-dividend_dict = transform_date_rate_as_dict(dividend_df, 'ex_date', 'amount')
-cpi_dict = transform_date_rate_as_dict(cpi_df, 'date', 'inflation_rate')
-
-earliest = dividend_df.iloc[-1]['ex_date']
-
-
-def get_rates_by_date(row):
-    date = row['date']
-    if date >= earliest:
-        cash_rate = get_rate_by_date(date, cash_rate_dict)
-        dividend = get_rate_by_date(date, dividend_dict)
-        cpi = get_rate_by_date(date, cpi_dict)
-    else:
-        cash_rate = 0.0
-        dividend = 0.0
-        cpi = 0.0
-
-    row['cash_rate'] = cash_rate
-    row['dividend'] = dividend
-    row['cpi'] = cpi
+def collate_qis(row, qis):
+    date = row.date
+    for qi_name, qi in qis.items():
+        row[qi_name] = get_rate(date, qi)
 
     return row
 
 
-enrich_tls_df = tls_df.apply(get_rates_by_date, axis=1)
-enrich_tls_df = enrich_tls_df.loc[enrich_tls_df['date'] >= earliest][
-    ['close', 'volume', 'cash_rate', 'dividend', 'cpi']]
+def collate(asx_code):
+    imfs = imf_ingest(imf_filenames, imf_ingest_params)
+    cash = cash_ingest(cash_ingest_params)
+    div = div_ingest(asx_code, div_ingest_params)
 
-X = enrich_tls_df[['volume', 'cash_rate', 'dividend', 'cpi']]
-Y = enrich_tls_df['close']
+    qis = imfs
+    qis['cash'] = cash
+    qis['div'] = div
 
-reg = linear_model.BayesianRidge()
-reg.fit(X, Y)
+    share = share_ingest(asx_code, share_ingest_params)
 
-# predict the share price by giving the other factors.
-vol = 28400448
-cr = 0.25
-div = 0.08
-ir = 2.5
+    collated_share = share.apply(lambda row: collate_qis(row, qis), axis=1)
 
-x = [vol, cr, div, ir]
-y = reg.predict([x])
-print(y)
+    return collated_share
+
+
+def main():
+    collated_share = collate('tls')
+    # collated_share.to_csv("data/{asx_code}_collated.csv".format(asx_code="tls"))
+    X = collated_share[['volume', 'cpi', 'gdp', 'unemployment', 'current', 'government', 'cash', 'div']]
+    Y = collated_share['close'].values
+
+    reg = linear_model.BayesianRidge()
+    reg.fit(X, Y)
+
+    # predict the share price by giving the other factors.
+    vol = 28400448
+    cpi = 1.5
+    gdp = 1.2
+    unemployment = 8.5
+    current = -3
+    government = -2.5
+    cash = 0.25
+    div = 8
+
+    x = [vol, cpi, gdp, unemployment, current, government, cash, div]
+    y = reg.predict([x])
+    print(y)
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
