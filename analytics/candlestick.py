80cd1966cb0fc25574858b7f3ffb424599288665
Miao Xie <miaoxie7788@gmail.com>
2020-09-24 10:15:24 +1000

    Fix a bug in is_hammer().

--- analytics/candlestick.py
+++ analytics/candlestick.py
@@ -46,6 +46,25 @@ def is_hammer(daily_price, low_th=0.02, body_th=0.5, high_th=0.01):
     """
     y1, y2, y3, y4 = daily_price['open'], daily_price['close'], daily_price['high'], daily_price['low']
 
+    d1 = y3 - y2
+    d2 = y2 - y1
+    d3 = y1 - y4
+
+    if d2 > 0:
+        if (d3 / y1 >= low_th) and (d2 / d3 <= body_th) and (d1 / y2 <= high_th):
+            return True
+
+    return False
+
+
+def is_inverted_hammer(daily_price, low_th=0.02, body_th=0.5, high_th=0.01):
+    """
+        A black or a white candlestick in an upside-down hammer position.
+
+
+    """
+    y1, y2, y3, y4 = daily_price['open'], daily_price['close'], daily_price['high'], daily_price['low']
+
     d1 = y3 - y1
     d2 = y1 - y2
     d3 = y2 - y4
@@ -55,9 +74,6 @@ def is_hammer(daily_price, low_th=0.02, body_th=0.5, high_th=0.01):
             return True
 
     return False
-
-
-def is_inverted_hammer():
     pass
 
 
