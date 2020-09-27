a1a871cdbbb4fa1d743f9a0fb6ce4a9434ce0cf3
Miao Xie <miaoxie7788@gmail.com>
2020-09-27 13:27:37 +1000

    Add test_is_big_black_candle().

--- tech_analysis/tests/unit/test_candlestick_pattern.py
+++ tech_analysis/tests/unit/test_candlestick_pattern.py
@@ -1,19 +1,35 @@
 import unittest
 
-from tech_analysis.candlestick.pattern import is_hammer, is_inverted_hammer
+from tech_analysis.candlestick.pattern import is_hammer, is_inverted_hammer, \
+    is_big_black_candle
 
 
 class TestCandlestickPattern(unittest.TestCase):
 
-    def test_is_hammer(self):
-        price = {'open': 5.0, 'close': 5.3, 'high': 5.1, 'low': 4.3}
+    def test_is_bullish_hammer(self):
+        candlestick = {'open': 5.1, 'close': 5.3, 'high': 5.35, 'low': 4.3}
 
-        self.assertEqual(is_hammer(price), True)
+        self.assertEqual(is_hammer(candlestick, t1=0.01, t2=0.05, t3=0.05), True)
 
-    def test_is_inverted_hammer(self):
-        price = {'open': 5.0, 'close': 5.2, 'high': 5.8, 'low': 4.95}
+    def test_is_bearish_hammer(self):
+        candlestick = {'open': 5.3, 'close': 5.1, 'high': 5.35, 'low': 4.3}
 
-        self.assertEqual(is_inverted_hammer(price), True)
+        self.assertEqual(is_hammer(candlestick, t1=0.01, t2=0.05, t3=0.05), True)
+
+    def test_is_bullish_inverted_hammer(self):
+        candlestick = {'open': 5.1, 'close': 5.3, 'high': 5.9, 'low': 5.05}
+
+        self.assertEqual(is_inverted_hammer(candlestick, t1=0.05, t2=0.05, t3=0.01), True)
+
+    def test_is_bearish_inverted_hammer(self):
+        candlestick = {'open': 5.3, 'close': 5.1, 'high': 5.9, 'low': 5.05}
+
+        self.assertEqual(is_inverted_hammer(candlestick, t1=0.05, t2=0.05, t3=0.01), True)
+
+    def test_is_big_black_candle(self):
+        candlestick = {'open': 5.0, 'close': 4.2, 'high': 5.02, 'low': 4.18}
+
+        self.assertEqual(is_big_black_candle(candlestick, t1=0.01, t2=0.05, t3=0.01), True)
 
 
 if __name__ == '__main__':
