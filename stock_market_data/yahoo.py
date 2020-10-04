76e055a4c825e5765ed2146189cafcb815df5654
Miao Xie <miaoxie7788@gmail.com>
2020-10-05 00:54:05 +1100

    Change to simplify get_stock_historical_data().

--- stock_market_data/yahoo.py
+++ stock_market_data/yahoo.py
@@ -3,91 +3,61 @@
 """
 
 import os
-from datetime import datetime, timedelta
 
 from yahoo_fin.stock_info import get_data, get_dividends, get_splits
 
 
-def get_stock_historical_data(stock_code, stock_path="data/asx_stock/csv"):
+def get_stock_historical_data(stock_code, start_date=None, end_date=None, path="data/asx_stock/csv"):
     """
-        Get historical price, dividend and splits data for a stock.
+        Get historical price, dividend and splits data for a stock between start_date and end_date.
     """
-    # Create a folder for the stock.
-    path = os.path.join(stock_path, stock_code)
-    if not os.path.exists(path):
-        os.mkdir(path)
+    # Make a folder for the stock if it does not exist.
+    stock_path = os.path.join(path, stock_code)
+    if not os.path.exists(stock_path):
+        os.mkdir(stock_path)
 
-    csv_filenames = os.listdir(path)
-    for data_type in ["price", "dividend", "splits"]:
-        # Get latest end_date. filename pattern: hist_price_{start date}_{end date}
-        if csv_filenames:
-            latest_end_date = max([str(csv_filename).split('.')[0].split('_')[-1] for csv_filename in csv_filenames
-                                   if data_type in csv_filename])
-        else:
-            latest_end_date = None
+    # Get historical data.
+    stock_info_func_dict = {
+        "price": get_data,
+        "dividend": get_dividends,
+        "splits": get_splits
+    }
 
-        # Get hist data between latest_end_date and today.
-        if latest_end_date:
-            start_date = datetime.strptime(latest_end_date, "%Y%m%d") + timedelta(days=1)
-        else:
-            start_date = None
+    for data_type in stock_info_func_dict:
+        try:
+            df = stock_info_func_dict[data_type](ticker=stock_code,
+                                                 start_date=start_date,
+                                                 end_date=end_date,
+                                                 index_as_date=False)
 
-        end_date = datetime.today()
-        if start_date and start_date >= end_date:
-            raise Exception("The historical price data until {end_date} has been downloaded.".format(end_date=end_date))
+            # Get start_date and end_date.
+            start_date = df["date"].min()
+            end_date = df["date"].max()
 
-        df = None
-        if data_type == "price":
-            if start_date:
-                df = get_data(stock_code, start_date=start_date, end_date=end_date, index_as_date=False)
-            else:
-                df = get_data(stock_code, end_date=end_date, index_as_date=False)
-                start_date = df["date"].min()
-        elif data_type == "dividend":
-            try:
-                get_dividends(stock_code)
-            except KeyError:
-                print("{stock_code} does not have historical dividend data.".format(stock_code=stock_code))
-            except AssertionError:
-                print("{stock_code} does not have historical dividend data.".format(stock_code=stock_code))
-            else:
-                if start_date:
-                    df = get_dividends(stock_code, start_date=start_date, end_date=end_date, index_as_date=False)
-                else:
-                    df = get_dividends(stock_code, end_date=end_date, index_as_date=False)
-                    start_date = df["date"].min()
-        elif data_type == "splits":
-            try:
-                get_splits(stock_code)
-            except KeyError:
-                print("{stock_code} does not have historical splits data.".format(stock_code=stock_code))
-            except AssertionError:
-                print("{stock_code} does not have historical splits data.".format(stock_code=stock_code))
-            else:
-                if start_date:
-                    df = get_splits(stock_code, start_date=start_date, end_date=end_date, index_as_date=False)
-                else:
-                    df = get_splits(stock_code, end_date=end_date, index_as_date=False)
-                    start_date = df["date"].min()
-        else:
-            raise Exception("Unknown data type.")
+            # Make the filename for the historical data.
+            filename = os.path.join(stock_path, "hist_{date_type}_{start_date}_{end_date}.csv".
+                                    format(date_type=data_type,
+                                           start_date=start_date.strftime("%Y%m%d"),
+                                           end_date=end_date.strftime("%Y%m%d")))
 
-        # Export the csv to data/asx_stock/{asx_code}
-        if df is not None:
-            csv_filename = "hist_" + data_type + "_" + start_date.strftime("%Y%m%d") + "_" + end_date.strftime(
-                "%Y%m%d") + ".csv"
-            print(csv_filename)
-            df.to_csv(os.path.join(path, csv_filename), header=True, index=False)
-
-
-def get_asx_stock_historical_data(asx_stock_codes):
-    for asx_stock_code in asx_stock_codes:
-        print("\nStart downloading historical data for {stock_code}".format(stock_code=asx_stock_code))
-        get_stock_historical_data(asx_stock_code)
-        print("The historical data for {stock_code} has been downloaded. \n".format(stock_code=asx_stock_code))
+            if not os.path.exists(filename):
+                df.to_csv(filename, header=True, index=False)
+                print("Historical {data_type} data are downloaded for {stock_code} between {start_date} and {end_date}".
+                      format(stock_code=stock_code, data_type=data_type, start_date=start_date, end_date=end_date))
+        except KeyError:
+            print("Historical {data_type} data are not available for {stock_code}.".
+                  format(stock_code=stock_code, data_type=data_type))
 
 
 if __name__ == "__main__":
-    # Get asx stock historical price data.
-    asx_stock_watchlist = ["tls.ax", "wbc.ax", "nov.ax", "cba.ax", "hack.ax", "ltr.ax"]
-    get_asx_stock_historical_data(["300369.SZ"])
+    # Get asx stock historical data.
+    # asx_stock_watchlist = ["tls.ax", "wbc.ax", "nov.ax", "cba.ax", "hack.ax", "ltr.ax"]
+
+    # Get SZ stock historical data.
+
+    sz_stocks = ["300655.SZ", "300122.SZ", "002007.SZ", "300185.SZ", "002594.SZ", "002625.SZ",
+                 "000625.SZ", "300750.SZ", "000333.SZ", "002475.SZ", "000725.SZ", "300730.SZ", "002230.SZ",
+                 "000002.SZ", "002285.SZ", "000656.SZ", "000011.SZ", "000069.SZ", "000006.SZ", "300369.SZ"]
+
+    for stock in sz_stocks:
+        get_stock_historical_data(stock, path="data/sz_stock/csv")
