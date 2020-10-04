675b790235f9121a6b2b1390de3435a85b1b7b4f
Miao Xie <miaoxie7788@gmail.com>
2020-10-04 15:00:08 +1100

    Change date window range.

--- technical_analysis/candlestick/scan.py
+++ technical_analysis/candlestick/scan.py
@@ -2,17 +2,18 @@
     Scan candlestick patterns.
 """
 
-from technical_analysis.candlestick.signal import is_hammer_signal, is_inverted_hammer_signal
+from technical_analysis.candlestick.signal import is_inverted_hammer_signal
 
 
 def scan_hammer(df, window_size=5, abs_slope=0.5, t1=4, t3=2, small_body=0.01):
     n = len(df)
     eval_dict = dict()
-    for x in range(window_size, n):
+    for x in range(window_size, n - window_size + 1):
         candlesticks = df.iloc[x - window_size:x].to_dict(orient="records")
         present_candlestick = candlesticks[-1]
-        if is_hammer_signal(candlesticks, abs_slope, t1, t3, small_body):
-            eval_dict[present_candlestick["date"]] = df.iloc[x - window_size:x + window_size]
+        eval_dict[present_candlestick["date"]] = df.iloc[x - window_size:x + window_size]
+        # if is_hammer_signal(candlesticks, abs_slope, t1, t3, small_body):
+        #     eval_dict[present_candlestick["date"]] = df.iloc[x - window_size:x + window_size]
 
     print("There are a total of {n} bullish patterns.".format(n=len(eval_dict)))
 
@@ -22,7 +23,7 @@ def scan_hammer(df, window_size=5, abs_slope=0.5, t1=4, t3=2, small_body=0.01):
 def scan_inverted_hammer(df, window_size=5, abs_slope=0.5, t1=2, t3=4, small_body=0.01):
     n = len(df)
     eval_dict = dict()
-    for x in range(window_size, n):
+    for x in range(window_size, n - window_size + 1):
         candlesticks = df.iloc[x - window_size:x].to_dict(orient="records")
         present_candlestick = candlesticks[-1]
         if is_inverted_hammer_signal(candlesticks, abs_slope, t1, t3, small_body):
