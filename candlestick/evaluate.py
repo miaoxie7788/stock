7d19dacf5fb60f86e983e295cbf68e3b40c36453
Miao Xie <miaoxie7788@gmail.com>
2020-10-11 22:22:44 +1100

    Move scan_hammer() and scan_inverted_hammer() to evaluate.py.

--- candlestick/evaluate.py
+++ candlestick/evaluate.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 import plotly.graph_objects as go
 
+from candlestick.core.signal import is_hammer_signal, is_inverted_hammer_signal
+
 
 def plot_candlestick(candlestick_df):
     fig = go.Figure(data=[go.Candlestick(
@@ -61,3 +63,52 @@ def evaluate_any_higher_price(windows, key="close", a_share=False):
 
 def evaluate_bullish_trend(windows, key="close", print_detail=True):
     pass
+
+
+def scan_hammer(candlestick_df, his_size, fut_size, abs_slope, t1, t3, small_body, enhanced):
+    """
+        Scan hammer signals in each window across the candlestick df.
+    """
+
+    n = len(candlestick_df)
+    windows = list()
+    for t in range(his_size, n - fut_size):
+        his_candlesticks = candlestick_df.iloc[t - his_size: t].to_dict(orient="records")
+        cur_candlestick = candlestick_df.iloc[t].to_dict()
+        fut_candlesticks = candlestick_df.iloc[t + 1: t + fut_size + 1].to_dict(orient="records")
+
+        if is_hammer_signal(cur_candlestick, his_candlesticks, abs_slope, t1, t3, small_body, enhanced):
+            signal = True
+        else:
+            signal = False
+
+        window = {"date": cur_candlestick["date"], "cur": cur_candlestick, "his": his_candlesticks,
+                  "fut": fut_candlesticks, "is_hammer_signal": signal}
+
+        windows.append(window)
+
+    return windows
+
+
+def scan_inverted_hammer(candlestick_df, his_size, fut_size, abs_slope, t1, t3, small_body, enhanced):
+    """
+        Scan inverted_hammer signals in each window across the candlestick df.
+    """
+    n = len(candlestick_df)
+    windows = list()
+    for t in range(his_size, n - fut_size):
+        his_candlesticks = candlestick_df.iloc[t - his_size: t].to_dict(orient="records")
+        cur_candlestick = candlestick_df.iloc[t].to_dict()
+        fut_candlesticks = candlestick_df.iloc[t + 1: t + fut_size + 1].to_dict(orient="records")
+
+        if is_inverted_hammer_signal(cur_candlestick, his_candlesticks, abs_slope, t1, t3, small_body, enhanced):
+            signal = True
+        else:
+            signal = False
+
+        window = {"date": cur_candlestick["date"], "cur": cur_candlestick, "his": his_candlesticks,
+                  "fut": fut_candlesticks, "is_inverted_hammer_signal": signal}
+
+        windows.append(window)
+
+    return windows
