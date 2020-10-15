2ab1cc6ddebc63bb10e9be6009cc4a97a040d7d6
Miao Xie <miaoxie7788@gmail.com>
2020-10-15 23:22:29 +1100

    Add export_stock_info_df_to_csv.

--- stock_market_data/yahoo.py
+++ stock_market_data/yahoo.py
@@ -4,45 +4,64 @@
 
 import os
 
-from yahoo_fin.stock_info import get_data
+from yahoo_fin.stock_info import get_data, get_dividends, get_splits
 
 
-def get_stock_historical_data(stock_code, start_date=None, end_date=None, path="data/asx_stock/csv"):
+def export_stock_info_df_to_csv(stock_info_df_dict, path="data"):
+    """
+        Export dfs presented in stock_info_df to csvs.
+    """
+    # Make a folder if path does not exist.
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+    for name, df in stock_info_df_dict.items():
+        df.to_csv(os.path.join(path, name), index=False, header=True)
+
+
+def get_stock_historical_data(stock_code, data_types, start_date=None, end_date=None):
     """
         Get historical price, dividend and splits data for a stock between start_date and end_date.
+
+    :param stock_code:      stock code, e.g., car.ax
+    :param data_types:      ["price", "dividend", "splits"]
+    :param start_date:      start date
+    :param end_date:        end date
+    :return:                stock_info_df_dict
     """
-    # Make a folder for the stock if it does not exist.
-    stock_path = os.path.join(path, stock_code)
-    if not os.path.exists(stock_path):
-        os.mkdir(stock_path)
 
     # Get historical data.
     stock_info_func_dict = {
         "price": get_data,
-        # "dividend": get_dividends,
-        # "splits": get_splits
+        "dividend": get_dividends,
+        "splits": get_splits
     }
-    for data_type in stock_info_func_dict:
+
+    stock_info_df_dict = dict()
+    for data_type in data_types:
+        stock_info_func = stock_info_func_dict[data_type]
         try:
-            df = stock_info_func_dict[data_type](ticker=stock_code,
-                                                 start_date=start_date,
-                                                 end_date=end_date,
-                                                 index_as_date=False)
+            df = stock_info_func(ticker=stock_code,
+                                 start_date=start_date,
+                                 end_date=end_date,
+                                 index_as_date=False)
 
             # Get start_date and end_date.
             start_date = df["date"].min()
             end_date = df["date"].max()
 
-            # Make the filename for the historical data.
-            filename = os.path.join(stock_path, "hist_{date_type}_{start_date}_{end_date}.csv".
-                                    format(date_type=data_type,
-                                           start_date=start_date.strftime("%Y%m%d"),
-                                           end_date=end_date.strftime("%Y%m%d")))
+            # Split a stock_code into code and market, e.g, car.ax -> car, ax.
+            code, market = stock_code.split(".")
+            name = "{market}_{code}_{date_type}_{start_date}_{end_date}.csv".format(
+                market=market,
+                code=code,
+                date_type=data_type,
+                start_date=start_date.strftime("%Y%m%d"),
+                end_date=end_date.strftime("%Y%m%d"))
+            stock_info_df_dict[name] = df
 
-            if not os.path.exists(filename):
-                df.to_csv(filename, header=True, index=False)
-                print("Historical {data_type} data are downloaded for {stock_code} between {start_date} and {end_date}".
-                      format(stock_code=stock_code, data_type=data_type, start_date=start_date, end_date=end_date))
+            print("Historical {data_type} data are downloaded for {stock_code} between {start_date} and {end_date}".
+                  format(stock_code=stock_code, data_type=data_type, start_date=start_date, end_date=end_date))
 
         except KeyError:
             print("Historical {data_type} data are not available for {stock_code} between the given dates".
@@ -51,6 +70,8 @@ def get_stock_historical_data(stock_code, start_date=None, end_date=None, path="
         except AssertionError:
             print("Historical {data_type} data are not available for {stock_code}.".
                   format(stock_code=stock_code, data_type=data_type))
+
+    return stock_info_df_dict
 
 
 if __name__ == "__main__":
