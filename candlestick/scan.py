ffdc9b5543281de5fd2afd74a515bdd7d959ae37
Miao Xie <miaoxie7788@gmail.com>
2020-10-21 00:01:24 +1100

    Add scan_bullish_hammer().

--- candlestick/scan.py
+++ candlestick/scan.py
@@ -35,14 +35,43 @@ def get_data(last_days=21, watchlist="data/candlestick/hs_watchlist", stock_path
         export_stock_info_df_to_csv(dfs, path=stock_path)
 
 
-# def scan_bullish_hammer(price_df, )
-#
+def scan_bullish_hammer(price_df, params):
+    """
+        params = {
+            "bullish_hammer_params": {
+                "hammer_params": {"t1": 1,
+                                  "t3": 2,
+                                  "small_body": 0.05},
+                "key": "low",
+                "abs_slope": 0.25,
+                "enhanced": True,
+            },
+            "ref_size": 5,
+            }
+    """
+    scan_date = price_df.iloc[-1]["date"]
+    stock_code = price_df.iloc[-1]["ticker"]
+    ref_size = params["ref_size"]
+    candlestick = price_df.iloc[-1].to_dict()
 
-def scan_daily_hammer_signal(params, watchlist="data/candlestick/hs_watchlist", stock_path="data/candlestick/stock"):
+    pattern = None
+    if len(price_df) - 1 < ref_size:
+        print("{stock_code} does not have {ref_size} ref candlesticks".format(ref_size=ref_size, stock_code=stock_code))
+    else:
+        ref_candlesticks = price_df.iloc[-ref_size - 1:-1].to_dict(orient="records")
+
+        if is_bullish_hammer(candlestick, ref_candlesticks, params["bullish_hammer_params"]):
+            print("A bullish hammer is found for {stock} on {day}".format(stock=stock_code, day=scan_date))
+            pattern = {"date": scan_date, "stock_code": stock_code, "pattern": "bullish_hammer"}
+
+    return pattern
+
+
+def scan_patterns(params, watchlist="data/candlestick/hs_watchlist", stock_path="data/candlestick/stock"):
     with open(watchlist) as f:
         stock_codes = [line.strip() for line in f.readlines()]
 
-    signals = list()
+    patterns = list()
     for stock_code in stock_codes:
         code, market = stock_code.split(".")
         # short_csv_filename.
@@ -58,60 +87,55 @@ def scan_daily_hammer_signal(params, watchlist="data/candlestick/hs_watchlist", 
             print("{stock} does not have data.".format(stock=stock_code))
             continue
 
-        # By default, it scans the last candlestick in the price_df.
-        ref_candlesticks = price_df.iloc[-params["ref_size"] - 1:].to_dict(orient="records")
-        candlestick = price_df.iloc[-1].to_dict()
+        pattern = scan_bullish_hammer(price_df, params)
+        if pattern:
+            patterns.append(pattern)
 
-        if is_bullish_hammer(candlestick, ref_candlesticks, hs_params["bullish_hammer_params"]):
-            print("A signal is found for {stock} on {day}".format(stock=stock_code, day=price_df.iloc[-1]["date"]))
-            signals.append({"date": price_df.iloc[-1]["date"], "stock_code": stock_code})
-        # else:
-        #     print("{stock} has been scanned.".format(stock=stock_code))
-
-    return signals
+    return patterns
 
 
 if __name__ == "__main__":
-    # get_data(watchlist="data/candlestick/hs_watchlist", last_days=14, stock_path="data/candlestick/stock")
+    get_data(watchlist="data/candlestick/hs_watchlist", last_days=14, stock_path="data/candlestick/stock")
 
     hs_params = {
         "bullish_hammer_params": {
-            "hammer_params": {"t1": 1,
-                              "t3": 2,
-                              "small_body": 0.1},
+            "hammer_params": {"t1": 3,
+                              "t3": 4,
+                              "small_body": 0.05},
             "key": "low",
-            "abs_slope": 0.05,
+            "abs_slope": 0.25,
             "enhanced": True,
         },
         "ref_size": 5,
     }
 
-    hs_signals = scan_daily_hammer_signal(params=hs_params,
-                                          watchlist="data/candlestick/hs_watchlist",
-                                          stock_path="data/candlestick/stock")
+    hs_patterns = scan_patterns(params=hs_params,
+                                watchlist="data/candlestick/hs_watchlist",
+                                stock_path="data/candlestick/stock")
 
-    pd.DataFrame(hs_signals).to_csv("data/candlestick/results/hammer_signal_hs_stock_{today}.csv".format(
+    pd.DataFrame(hs_patterns).to_csv("data/candlestick/results/hs_stock_patterns_{today}.csv".format(
         today=datetime.today().strftime("%Y%m%d")),
         index=False,
         header=True)
 
-    # get_data(watchlist="data/candlestick/asx_watchlist", last_days=14, stock_path="data/candlestick/stock")
-    #
-    # asx_params_dict = {
-    #     "his_size": 5,
-    #     "fut_size": 2,
-    #     "abs_slope": 0.02,
-    #     "t1": 1,
-    #     "t3": 2,
-    #     "small_body": 0.1,
-    #     "enhanced": True,
-    # }
-    #
-    # asx_signals = scan_daily_hammer_signal(params=asx_params_dict,
-    #                                        watchlist="data/candlestick/asx_watchlist",
-    #                                        stock_path="data/candlestick/stock")
-    #
-    # pd.DataFrame(asx_signals).to_csv("data/candlestick/results/hammer_signal_asx_stock_{today}.csv".format(
-    #     today=datetime.today().strftime("%Y%m%d")),
-    #     index=False,
-    #     header=True)
+    get_data(watchlist="data/candlestick/asx_watchlist", last_days=14, stock_path="data/candlestick/stock")
+
+    asx_params = {
+        "bullish_hammer_params": {
+            "hammer_params": {"t1": 1,
+                              "t3": 2,
+                              "small_body": 0.1},
+            "key": "low",
+            "abs_slope": 0.02,
+            "enhanced": True,
+        },
+        "ref_size": 5,
+    }
+    asx_patterns = scan_patterns(params=asx_params,
+                                 watchlist="data/candlestick/asx_watchlist",
+                                 stock_path="data/candlestick/stock")
+
+    pd.DataFrame(asx_patterns).to_csv("data/candlestick/results/asx_stock_patterns_{today}.csv".format(
+        today=datetime.today().strftime("%Y%m%d")),
+        index=False,
+        header=True)
